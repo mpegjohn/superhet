9b4ebc1012343b1f6f82c4e33851c5c9ed437b0f
john wilkinson <john_wilkinson3@sky.com>
2014-08-14 21:06:34 +0100

    Added test for padded circuit

--- tests/components/CapacitorTest.java
+++ tests/components/CapacitorTest.java
@@ -29,5 +29,31 @@ public class CapacitorTest {
 		
 		assertEquals(50E-12, capacitor.getValue(), 0.01E-12);
 	}
+	
+	@Test
+	public void padderTest() {
+		/*
+		 * Consists of a 100pF tuning cap
+		 * Tc 20pF
+		 * P 300pF
+		 * Tl 20pF
+		 */
+		
+		Capacitor tuning = new Capacitor(100E-12);
+		
+		Capacitor tc = new Capacitor(20E-12);
+		
+		Capacitor p = new Capacitor(300E-12);
+		
+		Capacitor tl = new Capacitor(20E-12);
+		
+		
+		Capacitor totalCap = new Capacitor(tuning.getValue());
+		totalCap.addParallel(tc);
+		totalCap.addSerial(p);
+		totalCap.addParallel(tl);
+		assertEquals(105.7E-12, totalCap.getValue(), 0.1E-12);
+
+	}
 
 }
