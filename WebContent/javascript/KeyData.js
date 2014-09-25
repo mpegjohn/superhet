d2304c3074d73aaacf8b4d00e522455265e1f40e
mpegjohn <john_wilkinson3@sky.com>
2014-09-25 12:49:56 +0100

    Added validation for all fields, and some default values for stray and if freq

--- WebContent/javascript/KeyData.js
+++ WebContent/javascript/KeyData.js
@@ -1,10 +1,25 @@
+
+$(document).ready(function() {
+
+	$("#cap_stray").val(8)
+	$("#if_frequency").val(455)
+	
+});
+
+
 function validateKeyData() {
 	
 	var upper_freq = $('#upper_frequency').val();
 	var lower_freq = $('#lower_frequency').val();
+	var if_freq = $('#if_frequency').val();
+	var cap_high = $('#cap_high').val();
+	var cap_low = $('#cap_low').val();
+	
 	
 	var upper_value;
 	var lower_value;
+
+	$(".error").remove()
 	
 	if (upper_freq == "") {
 		console.log("Upper freq not set");
@@ -30,13 +45,43 @@ function validateKeyData() {
 	
 	if(upper_value < lower_value) {
 		$("#lowerFreqUnits").after("<p class=\"error\">Upper freq < lower freq </p>");
-	}
-	
-	if (upper_freq < lower_freq) {
-		console.log("upper frew should be higher than lower freq");
-		$("#lowerFreqUnits").after("<p class=\"error\">Need to supply the data</p>");
 		return false;
 	}
+
+	
+	if (if_freq == "") {
+		console.log("IF freq not set");
+		$("#ifFreqUnits").after("<p class=\"error\">Need an IF freq</p>");
+		return false;
+	}
+
+	if (cap_high == "") {
+		console.log("Cap high not set");
+		$("#capHighUnits").after("<p class=\"error\">Need a capacitance here</p>");
+		return false;
+	}
+	else {
+		var upper_cap_unit = $("#capHighUnits").val();
+		
+		upper_value = multiplier(cap_high, upper_cap_unit);
+	}
+	
+	if (cap_low == "") {
+		console.log("Cap low not set");
+		$("#capLowUnits").after("<p class=\"error\">Need a capacitance here</p>");
+		return false;
+	}
+	else {
+		var lower_cap_unit = $("#capLowUnits").val();
+		
+		lower_value = multiplier(cap_low, lower_cap_unit);
+	}
+	
+	if(upper_value < lower_value) {
+		$("#capLowUnits").after("<p class=\"error\">High cap < low cap </p>");
+		return false;
+	}
+	
 	
 	return true;
 }
