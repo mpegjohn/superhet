b63334dd28bd2a7838c06c6c9a35aa03f4c3ba3f
mpegjohn <john_wilkinson3@sky.com>
2015-11-23 15:37:49 +0000

    started changing googleplot

--- src/tracking/googleChartPlot.java
+++ src/tracking/googleChartPlot.java
@@ -23,49 +23,54 @@ public class googleChartPlot extends DataSourceServlet {
 	@Override
 	public DataTable generateDataTable(Query query, HttpServletRequest request) {
 
-	Tracking trackingData = new Tracking();
+		Tracking trackingData = new Tracking();
 
-	trackingData.setUpperFreqUnit(request.getParameter("upperFreqUnit"));
-	trackingData.setLowerFreqUnit(request.getParameter("lowerFreqUnit"));
-	trackingData.setIfFreqUnit(request.getParameter("ifFreqUnit"));
-	trackingData.setCapHighUnit(request.getParameter("capHighUnit");
-	trackingData.setCapLowUnit(request.getParameter("capLowUnit");
-	trackingData.setCapStrayUnit(request.getParameter("capStrayUnit");
-	trackingData.setUpperFreq(Double.parseDouble(request.getParameter("upperfreq")));
-	trackingData.setLowerFreq(Double.parseDouble(request.getParameter("lowerfreq")));
-	trackingData.setIfFreq(Double.parseDouble(request.getParameter("iffreq")));
-	trackingData.setCapHigh(Double.parseDouble(request.getParameter("capHigh")));
-	trackingData.setCapLow(Double.parseDouble(request.getParameter("capLow")));
-	trackingData.setCapStray(Double.parseDouble(request.getParameter("capStray")));
+		trackingData.setUpperFreqUnit(request.getParameter("upperFreqUnit"));
+		trackingData.setLowerFreqUnit(request.getParameter("lowerFreqUnit"));
+		trackingData.setIfFreqUnit(request.getParameter("ifFreqUnit"));
+		trackingData.setCapHighUnit(request.getParameter("capHighUnit"));
+		trackingData.setCapLowUnit(request.getParameter("capLowUnit"));
+		trackingData.setCapStrayUnit(request.getParameter("capStrayUnit"));
+		trackingData.setUpperFreq(Double.parseDouble(request
+				.getParameter("upperfreq")));
+		trackingData.setLowerFreq(Double.parseDouble(request
+				.getParameter("lowerfreq")));
+		trackingData.setIfFreq(Double.parseDouble(request
+				.getParameter("iffreq")));
+		trackingData.setCapHigh(Double.parseDouble(request
+				.getParameter("capHigh")));
+		trackingData.setCapLow(Double.parseDouble(request
+				.getParameter("capLow")));
+		trackingData.setCapStray(Double.parseDouble(request
+				.getParameter("capStray")));
 
-                        trackingData.calculate();
-                        
-                        OscillatorCircuit osc = new OscillatorCircuit(trackingData);
-                        osc.calculate();
-                        
-                        SignalCircuit sig = new SignalCircuit(trackingData);
-                        sig.calculate();
-                        
-                        double signalFo[] = new double[100];
-                        double oscFo[] = new double[100];
-                        double trackError[] = new double[100];
-                        
-                        for(int i = 0; i < 100; i++)
-                        {
-                                double rotation = (double) i/100.0;
-                                signalFo[i] = sig.calculateFo(rotation);
-                                oscFo[i] = osc.calculateFo(rotation);
-                                
-                                trackError[i] = (oscFo[i] - signalFo[i]) - trackingData.getIfFreq(); 
-                        }
-                        
-                        Sweep sweep = new Sweep();
-                        sweep.setOscFo(oscFo);
-                        sweep.setSignalFo(signalFo);
-                        sweep.setTrackError(trackError);
-                        sweep.setOsc(osc);
-                        sweep.setSig(sig);
-                        sweep.setTrack(trackingData);
+		trackingData.calculate();
+
+		OscillatorCircuit osc = new OscillatorCircuit(trackingData);
+		osc.calculate();
+
+		SignalCircuit sig = new SignalCircuit(trackingData);
+		sig.calculate();
+
+		double signalFo[] = new double[100];
+		double oscFo[] = new double[100];
+		double trackError[] = new double[100];
+
+		for (int i = 0; i < 100; i++) {
+			double rotation = (double) i / 100.0;
+			signalFo[i] = sig.calculateFo(rotation);
+			oscFo[i] = osc.calculateFo(rotation);
+
+			trackError[i] = (oscFo[i] - signalFo[i]) - trackingData.getIfFreq();
+		}
+
+		Sweep sweep = new Sweep();
+		sweep.setOscFo(oscFo);
+		sweep.setSignalFo(signalFo);
+		sweep.setTrackError(trackError);
+		sweep.setOsc(osc);
+		sweep.setSig(sig);
+		sweep.setTrack(trackingData);
 
 		// Create a data table,
 		DataTable data = new DataTable();
@@ -82,17 +87,16 @@ public class googleChartPlot extends DataSourceServlet {
 		for (int i = 0; i < numPoints; i++) {
 			try {
 				data.addRowFromValues(sweepData.getSignalFo()[i],
-						sweepData.getTrackError()[i]
-						);
+						sweepData.getTrackError()[i]);
 			} catch (TypeMismatchException e) {
 				System.out.println("Invalid type!");
 			}
 		}
 		return data;
 	}
-	
-	 @Override
-	  protected boolean isRestrictedAccessMode() {
-	    return false;
-	  }
+
+	@Override
+	protected boolean isRestrictedAccessMode() {
+		return false;
+	}
 }
